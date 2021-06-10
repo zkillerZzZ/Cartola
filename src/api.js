7c4734446a190da1fae25306051057e8efd62621
zkillerZzZ <69115868+zkillerZzZ@users.noreply.github.com>
2021-06-10 19:48:58 -0300

    Proxy

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api_cartola = axios.create({
 
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/time/id/'
+    baseURL: 'https://cors-anywhere.herokuapp.com//https://api.cartolafc.globo.com/time/id/'
 });
 
 export default api_cartola;
