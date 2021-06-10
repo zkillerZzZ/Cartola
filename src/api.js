50427c1e15eff142611db923a70cf43dc5e9bdbc
zkillerZzZ <69115868+zkillerZzZ@users.noreply.github.com>
2021-06-10 01:34:40 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
+const api_cartola = axios.create({
+
     baseURL: 'https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/time/id/'
 });
 
-export default api;
+export default api_cartola;
